7c42cfdfddd91f71c318902027b0c433a3129803
jakobvarg <155798912+jakobvarg@users.noreply.github.com>
2024-02-16 14:52:12 +0530

    Select records for DB
    Select records from the database.

---
+++ PHP Practise/pdo_select_db.php
@@ -0,0 +1,27 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "mydbtest";
+
+
+try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $stmt = $conn->prepare("SELECT * FROM myguests");
+        $stmt->execute();
+
+        $stmt->setFetchMode(PDO::FETCH_NUM);
+
+        foreach($stmt as $val) {
+            echo "First Name: ".$val[1]."<br>Last Name: ".$val[2]."<br>Email: ".$val[3]."<br><br>";
+        }
+} catch(PDOException $e) {
+    echo $sql."<br>".$e->getMessage();
+}
+
+$conn = null;
+
